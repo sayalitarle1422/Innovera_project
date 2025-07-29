16a5184dabc53c938d5bf82540ea9c39d71117de
sayalitarle1422 <sayalitarle1405@gmail.com>
2025-07-29 23:14:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/hackathonDB");
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ MongoDB Connection Failed: ${error.message}`);
+    console.error(`MongoDB Connection Failed: ${error.message}`);
     process.exit(1);
   }
 };
